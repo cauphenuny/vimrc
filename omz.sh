9d540a18090084026fbb22507c5c365fb57a1ec5
cauphenuny <starseekist@gmail.com>
2025-03-07 14:14:40 +0800

    add omz

---
+++ omz.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+./zsh-plugin.sh
+
+cp default.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/theme/
